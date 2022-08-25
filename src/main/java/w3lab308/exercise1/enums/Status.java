0812cac4a3fd304d47d11bcb61476b6dd3c64e94
Ausar <victorperez2019@gmail.com>
2022-08-25 10:13:05 +0200

    last commit

---
+++ src/main/java/w3lab308/exercise1/enums/Status.java
@@ -0,0 +1,6 @@
+package w3lab308.exercise1.enums;
+
+public enum Status {
+
+    ACTIVE, LAPSED
+}
---
+++ src/main/java/w3lab308/exercise2/enums/Status.java
@@ -0,0 +1,6 @@
+package w3lab308.exercise2.enums;
+
+public enum Status {
+
+    ATTENDING, NOT_ATTENDING, NO_RESPONSE
+}
